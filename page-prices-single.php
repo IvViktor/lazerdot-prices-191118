1441500b7d30ada0cbc2b08169d9a6e273253303
Viktor Ivanchenko <viittyok1992@gmail.com>
2018-11-30 19:56:25 +0200

    added template for single roices page, added styles for back link

---
+++ page-prices-single.php
@@ -0,0 +1,49 @@
+<?php
+/*
+  Template Name: Prices single item
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
+
+<div id="content" class="prices-content">
+
+    <h1 class="page-title"><?php the_title(); ?></h1>
+    
+    <div class="container content-data">
+        <div class="row">
+            <div class="col-md-3">
+               <div class="prices-sidebar-header">
+                    Прайсы на услуги
+               </div>
+               <?php 
+                    wp_nav_menu(
+                        array(
+                           'theme_location' => 'left_sidebar_menu',
+                           'menu_class' => 'side-menu',
+                           'menu_id' => 'prices-menu',
+                           'depth' => 2
+                        )
+                    );
+               ?>
+            </div>
+            <div class="col-md-9">
+                 <div class="row">
+                     <div class="col-xs-12 content-intro">
+                         <a href="https://lazerdot.com.ua/praysy/" class="back-link">Назад к списку цен</a>
+                     </div>
+                     <div class="col-xs-12 prices-content">
+						<?php the_content();?>
+                     </div>
+                 </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
