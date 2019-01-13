31bd98eef3aab11dfd7eeecc5be30ea9710d4bef
Viktor Ivanchenko <viittyok1992@gmail.com>
2019-01-13 17:10:32 +0200

    added usp template tags to Reviews page template

--- page-vivan-reviews.php
+++ page-vivan-reviews.php
@@ -18,10 +18,16 @@
                     <a id="show-review-form" href=""><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Оставить свой отзыв</a>
                 </div>
 
-                
+                <?php if (function_exists('user_submitted_posts')) user_submitted_posts(); ?>
 
             </div>
 
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="reviews-content">
+                </div>
+            </div>
         </div>
     </div>
 
