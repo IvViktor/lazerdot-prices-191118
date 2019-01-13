f21c47e44195166031665f5caebb13dca02c433b
Viktor Ivanchenko <viittyok1992@gmail.com>
2019-01-13 16:09:06 +0200

    added files for Reviews page with USP plugin

---
+++ page-vivan-reviews.php
@@ -0,0 +1,31 @@
+<?php
+/*
+  Template Name: New Lazerdot Reviews
+ */
+?>
+
+<?php get_header(); ?>
+
+<div id="content">
+
+    <div class="container">
+        <h1 class="page-title"><?php the_title(); ?></h1>
+
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="text-center">
+                    <a id="show-review-form" href=""><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Оставить свой отзыв</a>
+                </div>
+
+                
+
+            </div>
+
+        </div>
+    </div>
+
+
+</div>
+
+<?php get_footer(); ?>
