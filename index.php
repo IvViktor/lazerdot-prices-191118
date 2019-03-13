3879b0c58f558ebf048960c1760b847628288918
Viktor Ivanchenko <viittyok1992@gmail.com>
2019-03-13 23:20:06 +0200

    added markup for services icons block on main page

--- index.php
+++ index.php
@@ -24,6 +24,33 @@
         <div class="container">
             <h2 class="title">Услуги</h2>
 
+            <div class="index-services-section">
+                <div class="row">
+                    <div class="col-md-6 services-item">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="item-icon"></div>
+                            </div>
+                            <div class="col-sm-8 item-content">
+                                <h3>ITEM HEADER</h3>
+                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 services-item">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="item-icon"></div>
+                            </div>
+                            <div class="col-sm-8 item-content">
+                                <h3>ITEM HEADER</h3>
+                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <ul class="list-blog">
                 <li>
                     <div class="lblog-photo">
