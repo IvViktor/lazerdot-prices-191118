ea4c1d37c289c298fca9dcb75b21b48d006039e3
Viktor Ivanchenko <viittyok1992@gmail.com>
2019-03-15 22:22:04 +0200

    fixed styles for index page services section

--- index.php
+++ index.php
@@ -26,25 +26,29 @@
 
             <div class="index-services-section">
                 <div class="row">
-                    <div class="col-md-6 services-item">
+                    <div class="col-xs-6 services-item">
                         <div class="row">
-                            <div class="col-sm-4">
-                                <div class="item-icon"></div>
+                            <div class="col-md-4">
+                                <a class="item-icon" href="">
+                                    <img src="<?php bloginfo('template_url'); ?>/img/temp/s1.jpg" />
+                                </a>
                             </div>
-                            <div class="col-sm-8 item-content">
-                                <h3>ITEM HEADER</h3>
-                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <div class="col-md-8 item-content">
+                                <h3>Item header</h3>
+                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6 services-item">
+                    <div class="col-xs-6 services-item">
                         <div class="row">
-                            <div class="col-sm-4">
-                                <div class="item-icon"></div>
+                            <div class="col-md-4">
+                                <a class="item-icon" href="">
+                                    <img src="<?php bloginfo('template_url'); ?>/img/temp/s1.jpg" />
+                                </a>
                             </div>
-                            <div class="col-sm-8 item-content">
+                            <div class="col-md-8 item-content">
                                 <h3>ITEM HEADER</h3>
-                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                             </div>
                         </div>
                     </div>
